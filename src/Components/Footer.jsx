f2b243d4084ab49b5f09fcd85b075a2a5114e675
Sachin Kumar <sachinkumarskrose@gmail.com>
2021-07-31 11:32:40 +0530

    removed contact page

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
     <Box bg={"gray.100"} w="100%" p={4}>
       <Flex>
         <Link href="/about"><a><Text fontSize={{base:"xs", md:"sm", lg:"md", xl:"lg"}} mr={"4"} fontWeight={"medium"} textColor={"gray.700"} _hover={{textColor:"gray.400"}}>About Us</Text></a></Link>
-        <Link href="/contact"><a><Text fontSize={{base:"xs", md:"sm", lg:"md", xl:"lg"}} mr={"4"} fontWeight={"medium"} textColor={"gray.700"} _hover={{textColor:"gray.400"}}>Contact Us</Text></a></Link>
+        {/* <Link href="/contact"><a><Text fontSize={{base:"xs", md:"sm", lg:"md", xl:"lg"}} mr={"4"} fontWeight={"medium"} textColor={"gray.700"} _hover={{textColor:"gray.400"}}>Contact Us</Text></a></Link> */}
         <Link href="/privacy-policy"><a><Text fontSize={{base:"xs", md:"sm", lg:"md", xl:"lg"}} mr={"4"} fontWeight={"medium"} textColor={"gray.700"} _hover={{textColor:"gray.400"}}>Privacy Policy</Text></a></Link>
         <Spacer/>
         <Link href="/"><a><Text fontSize={{base:"xs", md:"sm", lg:"md", xl:"lg"}} fontWeight={"medium"} textColor={"gray.700"} _hover={{textColor:"gray.400"}}>Desinged By: Pal Labs</Text></a></Link>
